273357b3c9cce34face9aee50dc718f5748422d9
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-10-10 13:01:33 -0700

    Added error handling to message form

--- src/component/contact/index.js
+++ src/component/contact/index.js
@@ -1,5 +1,8 @@
 import './_contact.scss';
 import React from 'react';
+import superagent from 'superagent';
+
+import * as util from '../../lib/util.js';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -7,12 +10,17 @@ class Contact extends React.Component {
 
     this.state = {
       name: '',
-      email: '',
+      sender: '',
       subject: '',
-      content: '',
+      body: '',
+      nameError: '',
+      senderError: '',
+      bodyError: '',
+      subjectError: '',
       error: false,
     }
     this.onChange = this.onChange.bind(this);
+    this.errorCheck = this.errorCheck.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
   }
 
@@ -20,9 +28,38 @@ class Contact extends React.Component {
     let {name, value} = e.target;
     this.setState({[name]: value});
   }
+  errorCheck() {
+    let [{name, subject, body, sender}] = [this.state];
+    var nameError, subjectError, bodyError, senderError, error;
+    nameError = name === '' ? 'inputError' : '';
+    bodyError = body === '' ? 'inputError' : '';
+    subjectError = subject === '' ? 'inputError' : '';
+    senderError = sender === '' ? 'inputError' : '';
+
+    error = !nameError || !bodyError || !subjectError || !senderError ?
+    false: true;
+    this.setState({
+      nameError,
+      bodyError,
+      subjectError,
+      senderError,
+      error
+    });
+  }
+  compileMessage() {
+
+  }
 
   sendMessage(e) {
     e.preventDefault();
+    let to = 'edwinjdelrio@gmail.com';
+
+    this.errorCheck();
+    if(this.state.error) return undefined;
+
+    let data = this.compileMessage();
+
+
   }
   render() {
     return(
@@ -31,6 +68,7 @@ class Contact extends React.Component {
         <form onSubmit={this.sendMessage}>
           <input
             type='text'
+            className={this.state.nameError}
             name='name'
             placeholder='Pleas Enter Your Name.'
             value={this.state.name}
@@ -38,27 +76,33 @@ class Contact extends React.Component {
           />
           <input
             type='text'
-            name='email'
+            name='sender'
+            className={this.state.senderError}
             placeholder='Please Enter Your Email'
-            value={this.state.email}
+            value={this.state.sender}
             onChange={this.onChange}
           />
           <input
             type='text'
             name='subject'
+            className={this.state.subjectError}
             placeholder='Please Enter a Subject'
             value={this.state.subject}
             onChange={this.onChange}
           />
           <textarea
-            name='content'
+            name='body'
             type='text'
+            className={this.state.bodyError}
             placeholder='Write Message Here'
-            value={this.content}
+            value={this.body}
             onChange={this.onChange}
           ></textarea>
           <button type='submit'>Send Message</button>
         </form>
+        {util.renderIf(this.state.error,
+          <p>Please Fill Out All Fields!!</p>
+        )}
       </section>
     )
   }
